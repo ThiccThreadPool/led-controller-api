bb7559b3c21263a84bfab84be369b865938881bf
I583075 <rafal.szygula@sap.com>
2025-10-31 14:39:31 +0100

    esp32 controller program

---
+++ esp-32-impl/esp32-led-controller/esp32-led-controller.ino
@@ -0,0 +1,64 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "xxx";
+const char* password = "xxx";
+const char* mqtt_server = "xxx";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String msg;
+  for (int i = 0; i < length; i++) {
+    msg += (char)payload[i];
+  }
+  Serial.println(msg);
+
+  if (String(topic) == "led/control") {
+    if (msg == "ON") {
+      digitalWrite(5, HIGH);
+    } else if (msg == "OFF") {
+      digitalWrite(5, LOW);
+    }
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+      client.subscribe("led/control");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(5, OUTPUT);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");
+  delay(5000);
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
